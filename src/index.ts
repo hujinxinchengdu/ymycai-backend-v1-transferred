961c1a29e112d1646cdcfaa6ff6bb745fc292db5
Zhenghao Lin (Ethan) <gyouza_azuoyg@outlook.com>
2023-06-22 00:09:43 -0700

    Prettify some code

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response } from 'express';
 const port = 8000;
 
 const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("HELLO FROM EXPRESS + TS!!!!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('HELLO FROM EXPRESS + TS!!!!');
 });
 
-app.get("/hi", (req: Request, res: Response) => {
-  res.send("BYEEE!!");
+app.get('/hi', (req: Request, res: Response) => {
+  res.send('BYEEE!!');
 });
 
 app.listen(port, () => {
