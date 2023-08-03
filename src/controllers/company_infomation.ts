3ddfa006f2637bcdd746a193feeedc248ae50bef
David Hu <hujinxin0607@gmail.com>
2023-08-02 21:30:52 -0700

    install uuid for typescript, fix feature getListOfCompanyInfoAndTags

---
+++ src/controllers/company_infomation.ts
@@ -0,0 +1,86 @@
+import { AppDataSource } from '../configuration';
+import { Company } from '../models';
+
+interface TagInfo {
+  tag_id: string;
+  tag_cn: string;
+  tag_en: string;
+}
+
+interface CompanyInfo {
+  company_name: string;
+  company_symbol: string;
+  company_information: string;
+  industry_position: string;
+  tags: TagInfo[];
+}
+
+interface CompanyInfoList {
+  companies: CompanyInfo[];
+}
+
+async function getCompanyInfoAndTags(
+  companySymbol: string,
+): Promise<CompanyInfo> {
+  try {
+    const company = await AppDataSource.manager.findOne(Company, {
+      where: { company_symbol: companySymbol },
+      relations: ['tags'],
+    });
+
+    if (!company) {
+      throw new Error(`Company with symbol ${companySymbol} not found`);
+    }
+
+    return {
+      company_name: company.company_name,
+      company_symbol: company.company_symbol,
+      company_information: company.company_information,
+      industry_position: company.industry_position,
+      tags: company.tags.map((tag) => ({
+        tag_id: tag.tag_id,
+        tag_cn: tag.tag_cn,
+        tag_en: tag.tag_en,
+      })),
+    };
+  } catch (error) {
+    throw new Error(`Error while fetching company: ${error.message}`);
+  }
+}
+
+async function getListOfCompanyInfoAndTags(
+  companyList: string[],
+): Promise<CompanyInfoList> {
+  try {
+    const companiesInfo = await Promise.all(
+      companyList.map(async (companySymbol) => {
+        const company = await AppDataSource.manager.findOne(Company, {
+          where: { company_symbol: companySymbol },
+          relations: ['tags'],
+        });
+
+        if (!company) {
+          throw new Error(`Company with symbol ${companySymbol} not found`);
+        }
+
+        return {
+          company_name: company.company_name,
+          company_symbol: company.company_symbol,
+          company_information: company.company_information,
+          industry_position: company.industry_position,
+          tags: company.tags.map((tag) => ({
+            tag_id: tag.tag_id,
+            tag_cn: tag.tag_cn,
+            tag_en: tag.tag_en,
+          })),
+        };
+      }),
+    );
+
+    return { companies: companiesInfo };
+  } catch (error) {
+    throw new Error(`Error while fetching companies: ${error.message}`);
+  }
+}
+
+export { getCompanyInfoAndTags, getListOfCompanyInfoAndTags };
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,2 @@
-export * from './example';
-export * from './News.usecase';
+export * from './company_infomation';
+export * from './news_usecase';
