c80e61173ef07a87d355a075e7aae0645d6022bc
David Hu <hujinxin0607@gmail.com>
2023-09-10 14:39:42 -0700

    delete env.Node_ENV

--- src/configuration/auth-middleware.ts
+++ src/configuration/auth-middleware.ts
@@ -3,16 +3,16 @@ import { config } from 'dotenv';
 
 import * as fs from 'fs';
 
-if (fs.existsSync(`.env.${process.env.NODE_ENV}`)) {
-  console.log(`Using .env.${process.env.NODE_ENV} file`);
-  config({ path: `.env.${process.env.NODE_ENV}` });
-} else {
-  console.error(
-    `The .env.${process.env.NODE_ENV} file corresponding to NODE_ENV=${process.env.NODE_ENV} is not found!`,
-  );
-  console.warn('Using default .env file');
-  config();
-}
+// if (fs.existsSync(`.env.${process.env.NODE_ENV}`)) {
+//   console.log(`Using .env.${process.env.NODE_ENV} file`);
+//   config({ path: `.env.${process.env.NODE_ENV}` });
+// } else {
+//   console.error(
+//     `The .env.${process.env.NODE_ENV} file corresponding to NODE_ENV=${process.env.NODE_ENV} is not found!`,
+//   );
+//   console.warn('Using default .env file');
+//   config();
+// }
 
 const authMiddleware = auth({
   issuerBaseURL: process.env.YMYC_ISSUER_BASE_URL,
